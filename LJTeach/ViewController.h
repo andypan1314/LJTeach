271be650a42298cfaa1a0aed2f6b5f862eae4951
Andy <andypan1314@gmail.com>
2015-06-05 20:28:32 +0800

    第一次提交代码

---
+++ LJTeach/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LJTeach
+//
+//  Created by Andy on 15/6/5.
+//  Copyright (c) 2015年 Water. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
